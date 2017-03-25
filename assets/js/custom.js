438146d245f3607a51d7bc6c77a41515c2596ac3
Vadim <web.vadimkolosov@gmail.com>
2017-03-25 20:24:39 +0200

    +/+

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,44 @@
-$(document).ready(function () {
-    $('.last-link-wrapper').hover(function (e) {
-      $(this).children('.header--dropdown-menu').stop(true, false).fadeToggle(200);
-      $(this).children('.header--dropdown-menu').css( "display", "flex" );
-      e.preventDefault();
-    });
-
-    $('.message-dropdown').hover(function(e) {
-      $(this).children('.header--message-box').stop(true, false).fadeToggle(200);
-      $(this).children('.header--message-box').css('display', 'block');
-      e.preventDefault();
-    });
+$(document).ready(function() {
+  DropDownMenu();
+  hamburgerActive();
+  sliDer();
 });
+
+function DropDownMenu() {
+  $('.last-link-wrapper').hover(function (e) {
+    $(this).children('.header--dropdown-menu').stop(true, false).fadeToggle(200);
+    $(this).children('.header--dropdown-menu').css( "display", "flex" );
+    e.preventDefault();
+  });
+
+  $('.message-dropdown').hover(function(e) {
+    if ($(window).width() > 1170 ){
+    $(this).children('.header--message-box').stop(true, false).fadeToggle(200);
+    $(this).children('.header--message-box').css('display', 'block');
+    e.preventDefault();
+    }
+  });
+}
+
+
+function hamburgerActive() {
+  $('.header--hamburger').on('click', function() {
+  	$('.header--hamburger').toggleClass('active-hamburger-icon');
+  	$('.header--link-wrapper').toggleClass('header--link-wrapper-active');
+  });
+}
+
+function sliDer() {
+
+$('.testimonials--wrapper').first().addClass('active-blockquote');
+$('.control-dot').first().addClass('active-dot');
+$('.control-dot').click(function() {
+  var $this = $(this),
+      $siblings = $this.parent().children(),
+      position = $siblings.index($this);
+
+    $('.testimonials--wrapper').removeClass('active-blockquote').eq(position).addClass('active-blockquote');
+    $siblings.removeClass('active-dot');
+    $this.addClass('active-dot');
+  });
+}
